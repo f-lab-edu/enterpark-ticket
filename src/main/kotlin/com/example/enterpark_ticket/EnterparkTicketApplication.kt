d556e1bb561759deccb2c43f923fb1874b62dea4
YuGyeong98 <kuk3097@naver.com>
2024-10-30 15:26:04 +0900

    initial commit

---
+++ src/main/kotlin/com/example/enterpark_ticket/EnterparkTicketApplication.kt
@@ -0,0 +1,11 @@
+package com.example.enterpark_ticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnterparkTicketApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnterparkTicketApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/enterpark_ticket/EnterparkTicketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.enterpark_ticket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnterparkTicketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
