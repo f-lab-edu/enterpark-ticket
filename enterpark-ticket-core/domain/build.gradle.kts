ac3ebca2d5b5bedb2e603b5c21b576bebfd4d9b0
YuGyeong98 <kuk3097@naver.com>
2024-11-05 15:00:57 +0900

    [chore] flyway 의존성 추가

--- enterpark-ticket-core/domain/build.gradle.kts
+++ enterpark-ticket-core/domain/build.gradle.kts
@@ -8,6 +8,7 @@ jar.enabled = true
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-mysql")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 }
