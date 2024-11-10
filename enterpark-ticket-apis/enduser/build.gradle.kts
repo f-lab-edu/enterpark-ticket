22d946e63d49d8272172ee73483c58faba7ffea1
YuGyeong98 <kuk3097@naver.com>
2024-11-10 22:44:03 +0900

    #3 [chore] api 관련 라이브러리 추가

--- enterpark-ticket-apis/enduser/build.gradle.kts
+++ enterpark-ticket-apis/enduser/build.gradle.kts
@@ -1,5 +1,16 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation(project(":enterpark-ticket-core"))
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    implementation(project(":enterpark-ticket-core:domain"))
 }
