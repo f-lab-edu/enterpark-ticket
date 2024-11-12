a03721e5ec4fb70a4745baf44c6197fbcaa98c9f
YuGyeong98 <kuk3097@naver.com>
2024-11-12 23:42:47 +0900

    #3 [refactor] user 컬럼 nullable, length 추가

--- enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/user/domain/User.kt
+++ enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/user/domain/User.kt
@@ -9,23 +9,31 @@ class User(
     @Column(unique = true)
     var oid: Long, // 회원 번호
 
+    @Column(nullable = false, length = 20)
     var name: String,
 
+    @Column(nullable = false, length = 255)
     var email: String,
 
+    @Column(nullable = false, length = 20)
     var phoneNumber: String,
 
+    @Column(nullable = false)
     var birthDate: LocalDate,
 
     @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false, length = 15)
     var gender: GenderType,
 
+    @Column(nullable = true, length = 255)
     var address: String? = null,
 
     @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false, length = 15)
     var role: RoleType = RoleType.USER,
 
     @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false, length = 15)
     var state: StateType = StateType.NORMAL,
 
     @Id
