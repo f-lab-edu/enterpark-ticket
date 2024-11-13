0b86d3cbe9ce8fb320fd2faa59480165cc396a08
YuGyeong98 <kuk3097@naver.com>
2024-11-13 23:28:29 +0900

    #3 [chore] webclient -> feignclient 라이브러리로 변경

--- enterpark-ticket-apis/enduser/build.gradle.kts
+++ enterpark-ticket-apis/enduser/build.gradle.kts
@@ -1,12 +1,16 @@
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.2")
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
