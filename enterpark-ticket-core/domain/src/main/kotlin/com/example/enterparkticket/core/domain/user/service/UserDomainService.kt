42616b7bdfdebc8688a3db588875f0dcfa0a7898
YuGyeong98 <kuk3097@naver.com>
2024-11-14 13:17:37 +0900

    #3 [feat] 이미 존재하는 회원 예외처리

--- enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/user/service/UserDomainService.kt
+++ enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/user/service/UserDomainService.kt
@@ -12,8 +12,15 @@ class UserDomainService(private val userRepository: UserRepository) {
 
     @Transactional
     fun registerUser(oAuthInfo: OAuthInfo, email: String, dto: RegisterUserDto): User {
+        validateUser(oAuthInfo)
         val user = dto.toUserEntity(oAuthInfo, email)
         userRepository.save(user)
         return user
+    }
+
+    private fun validateUser(oAuthInfo: OAuthInfo) {
+        if (userRepository.existsByOAuthInfo(oAuthInfo)) {
+            throw RuntimeException("이미 존재하는 회원입니다.") // todo exception
+        }
     }
 }
