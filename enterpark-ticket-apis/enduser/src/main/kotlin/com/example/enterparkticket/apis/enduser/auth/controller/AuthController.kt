267dc90bf1d78e493a0507cb1135d914664a1ccb
YuGyeong98 <kuk3097@naver.com>
2024-11-24 02:39:54 +0900

    #7 [feat] 회원 탈퇴 controller 구현

--- enterpark-ticket-apis/enduser/src/main/kotlin/com/example/enterparkticket/apis/enduser/auth/controller/AuthController.kt
+++ enterpark-ticket-apis/enduser/src/main/kotlin/com/example/enterparkticket/apis/enduser/auth/controller/AuthController.kt
@@ -3,6 +3,10 @@ package com.example.enterparkticket.apis.enduser.auth.controller
 import com.example.enterparkticket.apis.enduser.auth.dto.request.RegisterUserRequest
 import com.example.enterparkticket.apis.enduser.auth.dto.response.RegisterUserResponse
 import com.example.enterparkticket.apis.enduser.auth.service.RegisterUseCase
+import com.example.enterparkticket.apis.enduser.auth.service.WithdrawUseCase
+import com.example.enterparkticket.apis.enduser.config.jwt.dto.AuthDetails
+import com.example.enterparkticket.apis.enduser.config.jwt.dto.AuthUser
+import com.example.enterparkticket.apis.enduser.config.jwt.dto.toUserId
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.ResponseEntity
@@ -12,7 +16,10 @@ import org.springframework.web.bind.annotation.*
 @Validated
 @RestController
 @RequestMapping("/v1/auth")
-class AuthController(private val registerUseCase: RegisterUseCase) {
+class AuthController(
+    private val registerUseCase: RegisterUseCase,
+    private val withdrawUseCase: WithdrawUseCase,
+) {
 
     @PostMapping("/oauth/kakao/register")
     fun kakaoOAuthRegisterUser(
@@ -21,5 +28,11 @@ class AuthController(private val registerUseCase: RegisterUseCase) {
     ): ResponseEntity<RegisterUserResponse> {
         val response = registerUseCase.registerUser(code, request)
         return ResponseEntity.status(CREATED).body(response)
+    }
+
+    @PatchMapping("/oauth/kakao/withdrawal")
+    fun kakaoOAuthWithdrawUser(@AuthUser user: AuthDetails): ResponseEntity<String> {
+        withdrawUseCase.withdrawUser(user.toUserId())
+        return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.")
     }
 }
