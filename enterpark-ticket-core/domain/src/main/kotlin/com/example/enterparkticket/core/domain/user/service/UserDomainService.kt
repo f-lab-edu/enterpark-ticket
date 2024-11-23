599e3c89178533237c887d5fc13829f743ae66f5
YuGyeong98 <kuk3097@naver.com>
2024-11-24 00:42:35 +0900

    #6 [feat] 회원 정보 수정 domain service 구현

--- enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/user/service/UserDomainService.kt
+++ enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/user/service/UserDomainService.kt
@@ -3,12 +3,15 @@ package com.example.enterparkticket.core.domain.user.service
 import com.example.enterparkticket.core.domain.user.domain.OAuthInfo
 import com.example.enterparkticket.core.domain.user.domain.User
 import com.example.enterparkticket.core.domain.user.exception.AlreadyRegisterUserException
+import com.example.enterparkticket.core.domain.user.exception.NotFoundUserException
 import com.example.enterparkticket.core.domain.user.repository.UserRepository
 import com.example.enterparkticket.core.domain.user.service.dto.RegisterUserDto
+import com.example.enterparkticket.core.domain.user.service.dto.UpdateUserDto
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class UserDomainService(private val userRepository: UserRepository) {
 
     @Transactional
@@ -17,6 +20,18 @@ class UserDomainService(private val userRepository: UserRepository) {
         val user = dto.toUserEntity(oAuthInfo, email)
         userRepository.save(user)
         return user
+    }
+
+    @Transactional
+    fun updateUser(userId: Long, dto: UpdateUserDto) {
+        val user = findUserById(userId)
+        user.updateUser(dto.name, dto.email, dto.phoneNumber, dto.address)
+    }
+
+    fun findUserById(userId: Long): User {
+        return userRepository.findById(userId).orElseThrow {
+            NotFoundUserException()
+        }
     }
 
     private fun validateUser(oAuthInfo: OAuthInfo) {
