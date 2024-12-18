90e926f9a119ec24688944313b245510252c151b
YuGyeong98 <kuk3097@naver.com>
2024-12-19 08:35:43 +0900

    #11 [refactor] 에러 코드 메시지 한글로 변경

--- enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/common/exeption/GlobalErrorCode.kt
+++ enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/common/exeption/GlobalErrorCode.kt
@@ -10,7 +10,7 @@ enum class GlobalErrorCode(private val status: Int, private val message: String)
     INVALID_EMAIL(UNAUTHORIZED, "유효하지 않은 이메일 주소입니다."),
 
     SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 오류가 발생하였습니다."),
-    NOT_AVAILABLE_DISTRIBUTED_LOCK(INTERNAL_SERVER_ERROR, "DistributedLock is not available");
+    NOT_AVAILABLE_DISTRIBUTED_LOCK(INTERNAL_SERVER_ERROR, "락 획득에 실패하였습니다.");
 
     override fun getErrorDescription(): ErrorDescription {
         return ErrorDescription(name, message, status)
