1351b5de71ea90b85c8ad3d46bbf74b646ad1c4b
YuGyeong98 <kuk3097@naver.com>
2024-12-30 05:37:45 +0900

    #11 [feat] async taskExecutor 적용

--- enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/config/async/AsyncConfig.kt
+++ enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/config/async/AsyncConfig.kt
@@ -1,8 +1,23 @@
 package com.example.enterparkticket.core.domain.config.async
 
 import com.example.enterparkticket.core.domain.config.EnterparkTicketConfig
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.ThreadPoolExecutor
 
 @EnableAsync
 class AsyncConfig : EnterparkTicketConfig {
+
+    @Bean
+    fun eventTaskExecutor(): ThreadPoolTaskExecutor {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 10
+            queueCapacity = 50
+            maxPoolSize = 30
+            setThreadNamePrefix("event-async-")
+            setTaskDecorator(LoggingTaskDecorator())
+            setRejectedExecutionHandler(ThreadPoolExecutor.CallerRunsPolicy())
+        }
+    }
 }
