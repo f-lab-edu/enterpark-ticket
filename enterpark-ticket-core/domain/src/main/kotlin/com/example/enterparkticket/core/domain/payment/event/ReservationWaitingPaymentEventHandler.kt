e93d3c6f222c826a3a3079ebfd5020d9b50543c5
YuGyeong98 <kuk3097@naver.com>
2024-12-30 05:39:04 +0900

    #11 [feat] taskExecutor aop 구현 및 적용

--- enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/payment/event/ReservationWaitingPaymentEventHandler.kt
+++ enterpark-ticket-core/domain/src/main/kotlin/com/example/enterparkticket/core/domain/payment/event/ReservationWaitingPaymentEventHandler.kt
@@ -1,9 +1,9 @@
 package com.example.enterparkticket.core.domain.payment.event
 
+import com.example.enterparkticket.core.domain.common.aop.Async
 import com.example.enterparkticket.core.domain.common.aop.Transaction
 import com.example.enterparkticket.core.domain.reservation.event.ReservationWaitingPaymentEvent
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
 
@@ -12,9 +12,10 @@ private val logger = KotlinLogging.logger {}
 @Component
 class ReservationWaitingPaymentEventHandler {
 
-    @Async
     @TransactionalEventListener
-    fun handle(event: ReservationWaitingPaymentEvent) = Transaction.propagationRequiresNew {
-        logger.info { "티켓 예매 결제 하기 ${event.reservationIds}" }
+    fun handle(event: ReservationWaitingPaymentEvent) = Async.eventTaskExecutor {
+        Transaction.propagationRequiresNew {
+            logger.info { "티켓 예매 결제 하기 ${event.reservationIds}" }
+        }
     }
 }
